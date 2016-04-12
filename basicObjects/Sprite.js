fae7f4055068ce02a9ac87e2795c20480825e3f9
DivineOmega <jordan@hall05.co.uk>
2016-04-12 23:44:53 +0100

    Added Sprite object

---
+++ basicObjects/Sprite.js
@@ -0,0 +1,30 @@
+function Sprite() {
+
+  this.convergame = null;
+
+  this.x = 0;
+  this.y = 0;
+
+  this.imagePath = null;
+
+  this.update = function(time)
+  {
+
+  };
+
+  this.render = function()
+  {
+    this.convergame.draw.image(this.imagePath, this.x, this.y, this.width, this.height);
+  };
+
+  this.init = function(convergame, x, y, width, height, imagePath)
+  {
+      this.convergame = convergame;
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.imagePath = imagePath;
+  };
+
+}
