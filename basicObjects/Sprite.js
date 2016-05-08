b8265c8bd4dfee1125e81ec29f04b089b8ee3653
Ollie Reardon <ojreardon@gmail.com>
2016-05-08 15:34:10 +0100

    Expanded Sprite object functionality

--- basicObjects/Sprite.js
+++ basicObjects/Sprite.js
@@ -4,12 +4,61 @@ function Sprite() {
 
   this.x = 0;
   this.y = 0;
-
+  this.width = 0;
+  this.height = 0; 
   this.imagePath = null;
 
   this.render = function()
   {
     this.convergame.draw.image(this.imagePath, this.x, this.y, this.width, this.height);
+  };
+
+  this.setWidth = function (width) 
+  {
+    this.width = width;
+  }
+  this.setHeight = function(height)
+  {
+    this.height = height;
+  }
+  this.setX = function(x)
+  {
+    this.x = x;
+  };
+
+  this.setY = function(y)
+  {
+    this.y = y;
+  };
+
+  this.setImagePath = function(imagePath)
+  {
+    this.imagePath = imagePath;
+  };
+
+  this.getWidth = function()
+  {
+    return this.width;
+  }
+
+  this.getHeight = function()
+  {
+    return this.height;
+  }
+
+  this.getX = function()
+  {
+    return this.x;
+  };
+
+  this.getY = function()
+  {
+    return this.y;
+  };
+
+  this.getImagePath = function()
+  {
+    return this.imagePath;
   };
 
   this.init = function(convergame, x, y, width, height, imagePath)
